e1c80c0bdf3812e4f3d204a2f163c4ad4a091c65
shubham2028 <shubhamnalawade989@gmail.com>
2022-07-24 11:10:39 +0530

    Changes in HomeController

--- HomeController.java
+++ HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 	public HomeController() {
 		System.out.println("in ctor of "+getClass());
+System.out.println("Welcome!!!");
 	}
 	
 	@GetMapping("/")
